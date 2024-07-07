bb03761104a493c34d5b4696ce70da0d18525aa1
artiehumphreys <artieh2003@gmail.com>
2024-07-07 16:48:48 -0400

    fix: better lighting

--- src/Computer.js
+++ src/Computer.js
@@ -10,13 +10,35 @@ camera.position.set(0, 25, 75)
 const canvas = document.getElementById('canvas');
 const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.physicallyCorrectLights = true;
+renderer.shadowMap.enabled = true; 
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-const ambientLight = new THREE.AmbientLight(0xffffff, 2.5);
-directionalLight.position.set(0, 75, 75).normalize();
-directionalLight.target.position.set(0, 0, -25)
-scene.add(directionalLight);
-scene.add(directionalLight.target);
+const directionalLight1 = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight1.position.set(50, 75, 75);
+directionalLight1.target.position.set(0, 0, -10);
+directionalLight1.castShadow = true;
+directionalLight1.shadow.mapSize.width = 1024;
+directionalLight1.shadow.mapSize.height = 1024;
+directionalLight1.shadow.camera.near = 0.1;
+directionalLight1.shadow.camera.far = 500;
+
+const directionalLight2 = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight2.position.set(-50, 75, 75);
+directionalLight2.target.position.set(0, 0, -10);
+directionalLight2.castShadow = true;
+directionalLight2.shadow.mapSize.width = 1024;
+directionalLight2.shadow.mapSize.height = 1024;
+directionalLight2.shadow.camera.near = 0.1;
+directionalLight2.shadow.camera.far = 500;
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
+
+scene.add(directionalLight1);
+scene.add(directionalLight1.target);
+scene.add(directionalLight2);
+scene.add(directionalLight2.target);
 scene.add(ambientLight);
 
 loader.load(
