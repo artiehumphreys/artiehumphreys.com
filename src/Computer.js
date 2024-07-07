f9bae04c96354b65812552affc0d148a4ecb19ff
artiehumphreys <artieh2003@gmail.com>
2024-07-07 15:18:58 -0400

    fix: fixed incorrect three imports

--- src/Computer.js
+++ src/Computer.js
@@ -1,34 +1,31 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-
+import * as THREE from "three";
+import { GLTFLoader } from "GLTFLoader"
+const loader = new GLTFLoader();
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
 
-const renderer = new THREE.WebGLRenderer();
+const canvas = document.getElementById('canvas');
+const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
 
-const loader = new GLTFLoader();
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(1, 1, 1).normalize();
+scene.add(light);
 
-function load(){
-    const light = new THREE.DirectionalLight(0xffffff, 1);
-    light.position.set(1, 1, 1).normalize();
-    scene.add(light);
-    loader.load(
-        '../models/old_pc_low_poly_game_model.glb',
-        function ( gltf ) {
-            scene.add( gltf.scene );
-        },
-        function ( xhr ) {
-            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-        },
-        function ( error ) {
-            console.log( 'An error happened' );
-        }
-    );
-}
+loader.load(
+    '../models/old_pc_low_poly_game_model.glb',
+    function ( gltf ) {
+        scene.add( gltf.scene );
+    },
+    function ( xhr ) {
+        console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+    },
+    function ( error ) {
+        console.log( 'An error happened' );
+    }
+);
 
 function animate() {
     requestAnimationFrame(animate);
@@ -36,5 +33,4 @@ function animate() {
 }
 animate();
 
-load()
 
