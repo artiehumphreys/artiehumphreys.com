4b08d38d573244cad023b5359d258ff38f71f0a4
artiehumphreys <artieh2003@gmail.com>
2024-07-07 17:33:27 -0400

    fix: fixed margins with body element

--- src/animateCamera.js
+++ src/animateCamera.js
@@ -1,5 +1,5 @@
 export function animateCamera(camera) {
-    const startPosition = { x: 0, y: 50, z: 100, rotationX: -0.4 };
+    const startPosition = { x: 0, y: 50, z: 100, rotationX: -0.5 };
     const endPosition = { x: 0, y: 43, z: 38, rotationX: 0 };
 
     const tween = new TWEEN.Tween(startPosition)
