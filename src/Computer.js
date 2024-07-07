d7b338df7c444668edb333c4710ca388dddf5c90
artiehumphreys <artieh2003@gmail.com>
2024-07-07 14:46:44 -0400

    feat: created html file to display computer

--- src/Computer.js
+++ src/Computer.js
@@ -1,15 +1,25 @@
+import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
 const loader = new GLTFLoader();
+
 function load(){
+    const light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(1, 1, 1).normalize();
+    scene.add(light);
     loader.load(
         '../models/old_pc_low_poly_game_model.glb',
         function ( gltf ) {
             scene.add( gltf.scene );
-            gltf.animations;
-            gltf.scene;
-            gltf.scenes;
-            gltf.cameras;
-            gltf.asset;
         },
         function ( xhr ) {
             console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
@@ -19,6 +29,12 @@ function load(){
         }
     );
 }
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+animate();
 
 load()
 
