e67da4ba77138e15762f1b23208ed0b22e69fc6f
artiehumphreys <artieh2003@gmail.com>
2024-07-07 15:34:41 -0400

    feat: added lighting for scene

--- src/Computer.js
+++ src/Computer.js
@@ -1,18 +1,23 @@
 import * as THREE from "three";
 import { GLTFLoader } from "GLTFLoader"
+
 const loader = new GLTFLoader();
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+camera.position.set(0, 25, 75)
 
 const canvas = document.getElementById('canvas');
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-const light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(1, 1, 1).normalize();
-scene.add(light);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+directionalLight.position.set(0, 75, 75).normalize();
+directionalLight.target.position.set(0, 0, -25)
+scene.add(directionalLight);
+scene.add(directionalLight.target);
+scene.add(ambientLight);
 
 loader.load(
     '../models/old_pc_low_poly_game_model.glb',
@@ -32,5 +37,6 @@ function animate() {
     renderer.render(scene, camera);
 }
 animate();
+
 
 
