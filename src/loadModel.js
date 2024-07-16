35d4a57972ff20eab3436acd8adf4a53e705e445
artiehumphreys <artieh2003@gmail.com>
2024-07-16 14:20:13 -0400

    feat: icons on screen for different pages

--- src/loadModel.js
+++ src/loadModel.js
@@ -8,21 +8,13 @@ export function loadModel(scene) {
         model.traverse(function (node) {
             if (node.isMesh && node.name === "defaultMaterial") {
                 const screenMesh = node;
-                const screenGeometry = new THREE.PlaneGeometry(50, 34, 32, 32);
-                const vertices = screenGeometry.attributes.position.array;
-
-                for (let i = 0; i < vertices.length; i += 3) {
-                    const x = vertices[i];
-                    const y = vertices[i + 1];
-                    const offset = 3;
-                    vertices[i + 2] = offset + 2 + Math.cos((x / 65) * Math.PI) * offset - (y / 28) * offset * 0.35;
-                }
-                screenGeometry.attributes.position.needsUpdate = true;
-                screenGeometry.computeVertexNormals();
+                const width = 50;
+                const height = 34;
+                const screenGeometry = curvePlanes(width, height);
 
                 const screenMaterial = new THREE.MeshStandardMaterial({
                     map: screenTexture,
-                    roughness: 0.0,
+                    roughness: 0.1,
                     metalness: 0.0
                 });
                 const screen = new THREE.Mesh(screenGeometry, screenMaterial);
@@ -32,22 +24,59 @@ export function loadModel(scene) {
         });
     }
 
+    function curvePlanes(width, height){
+        const screenGeometry = new THREE.PlaneGeometry(width, height, 25, 17);
+        const vertices = screenGeometry.attributes.position.array;
+
+        for (let i = 0; i < vertices.length; i += 3) {
+            const x = vertices[i];
+            const y = vertices[i + 1];
+            const offset = 3;
+            vertices[i + 2] = offset + 2 + Math.cos((x / 65) * Math.PI) * offset - (y / 28) * offset * 0.35;
+        }
+
+        screenGeometry.attributes.position.needsUpdate = true;
+        screenGeometry.computeVertexNormals();
+        return screenGeometry
+
+    }
+
+    function createCenteredIcon(texture, positionX) {
+        const planeGeometry = curvePlanes(12.5, 8.5);
+        const planeMaterial = new THREE.MeshStandardMaterial({
+            map: texture,
+            roughness: 0.1,
+            metalness: 0.0,
+            transparent: true
+        });
+        const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+        plane.position.set(positionX, 42, 8.8);
+        scene.add(plane);
+    }
+
     const textureLoader = new THREE.TextureLoader();
     const screenTexture = textureLoader.load('../models/background.jpg');
+    const aboutMeTexture = textureLoader.load('../models/aboutme.png');
+    const contactTexture = textureLoader.load('../models/contact.png');
+    const experienceTexture = textureLoader.load('../models/experience.png');
+    const projectTexture = textureLoader.load('../models/projects.png');
     const loader = new GLTFLoader();
     loader.load(
         '../models/scene.glb',
         function (gltf) {
             scene.add(gltf.scene);
             const model = gltf.scene;
-            model = createScreen(model);
-            scene.add(model);
+            createScreen(model);
+            createCenteredIcon(aboutMeTexture, -15);
+            createCenteredIcon(contactTexture, 5);
+            createCenteredIcon(experienceTexture, 15);
+            createCenteredIcon(projectTexture, -5);
         },
         function (xhr) {
             console.log((xhr.loaded / xhr.total * 100) + '% loaded');
         },
         function (error) {
-            console.log('An error happened');
+            console.error('An error happened: ' + error);
         }
     );
 }
