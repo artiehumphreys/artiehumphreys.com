379e97da87e593b5c4b1d3af693435318c6cad39
artiehumphreys <artieh2003@gmail.com>
2024-07-17 13:33:20 -0400

    feat: webgl check for compatability

--- src/Computer.js
+++ src/Computer.js
@@ -4,42 +4,43 @@ import { loadModel } from "./loadComputerModel.js";
 import { animateCamera } from "./animateCamera.js";
 import { handleIconClickEvents } from "./handleIconClickEvents.js";
 
-const raycaster = new THREE.Raycaster();
-const mouse = new THREE.Vector2();
+export function computer(){
+    const raycaster = new THREE.Raycaster();
+    const mouse = new THREE.Vector2();
 
-const scene = new THREE.Scene();
+    const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 50, 100)
-camera.rotation.set(-0.5, 0, 0)
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set(0, 50, 100)
+    camera.rotation.set(-0.5, 0, 0)
 
-const canvas = document.getElementById('canvas');
-const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.outputEncoding = THREE.sRGBEncoding;
-renderer.physicallyCorrectLights = true;
-renderer.shadowMap.enabled = true; 
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-
-setupLights(scene);
-
-loadModel(scene);
-
-animateCamera(camera);
-
-function animate(time) {
-    requestAnimationFrame(animate);
-    TWEEN.update(time);
-    renderer.render(scene, camera);
-}
-animate();
-
-window.addEventListener('resize', () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+    const canvas = document.getElementById('canvas');
+    const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
-});
+    renderer.outputEncoding = THREE.sRGBEncoding;
+    renderer.physicallyCorrectLights = true;
+    renderer.shadowMap.enabled = true; 
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-window.addEventListener('click', handleIconClickEvents(scene, camera, raycaster, mouse), false);
+    setupLights(scene);
 
+    loadModel(scene);
+
+    animateCamera(camera);
+
+    function animate(time) {
+        requestAnimationFrame(animate);
+        TWEEN.update(time);
+        renderer.render(scene, camera);
+    }
+    animate();
+
+    window.addEventListener('resize', () => {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    });
+
+    window.addEventListener('click', handleIconClickEvents(scene, camera, raycaster, mouse), false);
+}
 
