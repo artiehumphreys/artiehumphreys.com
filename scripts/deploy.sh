45c08a8ac21efeef1fa3182a88eab7dc4f9df56c
artiehumphreys <artieh2003@gmail.com>
2024-08-18 16:25:06 -0400

    fix: remove unnecessary S3 files

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,9 +3,11 @@
 aws s3 sync . s3://artiehumphreys.com --delete \
     --exclude ".git/*" \
     --exclude ".github/*" \
-    --exclude "readme.md" \
+    --exclude "README.md" \
     --exclude ".DS_Store" \
     --exclude ".gitignore" \
     --exclude "eslint.config.mjs" \
     --exclude "node_modules/*" \
-    --exclude "scripts/*.sh"
+    --exclude "scripts/*.sh" \
+    --exclude "package.json" \
+    --exclude "package-lock.json" \
