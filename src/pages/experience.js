ea10623e9508ae523d49988c147151530c3cfffa
artiehumphreys <artieh2003@gmail.com>
2024-08-20 15:41:12 -0400

    fix: better spacing

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -5,43 +5,43 @@ export async function experiencePage(scene) {
 
   const textData = [
     {
-      position: [-6.4, 53.5, 8.7],
+      position: [-6.4, 53.75, 8.7],
       content: "Biocore LLC - Computer Vision Intern",
       name: "header",
     },
     {
-      position: [-1, 51.5, 8.7],
+      position: [-1, 51.25, 8.7],
       content:
         "Automated 3D animation workflows for multi-view analysis of player injuries in NBA games, \nshowcased at NBA Tech Expo 2024",
     },
     {
-      position: [-14.4, 48.5, 8.7],
+      position: [-14.4, 48.25, 8.7],
       content: "Skills: Go, Python, OpenCV, PyTorch, bpy",
       name: "skills",
     },
 
     {
-      position: [-7.2, 46, 8.7],
+      position: [-7.2, 45.25, 8.7],
       content: "Sitscape - Software Engineer Intern",
       name: "header",
     },
     {
-      position: [0, 44, 8.71],
+      position: [0, 42.75, 8.71],
       content:
         "Enhanced keyword and semantic search capabilities using Amazon OpenSearch, doubling search \nspeed and improving result accuracy",
     },
     {
-      position: [-12.85, 41, 8.7],
+      position: [-12.85, 39.75, 8.7],
       content: "Skills: Python, JavaScript, jQuery, AWS, Langchain",
       name: "skills",
     },
     {
-      position: [-2.6, 38.5, 8.7],
+      position: [-2.6, 36.75, 8.7],
       content: "UVA Men's Basketball Team - Student Manager",
       name: "header",
     },
     {
-      position: [-0.95, 36.5, 8.71],
+      position: [-0.95, 34.25, 8.71],
       content:
         "Dedicate 25 hours weekly to ensure the seamless execution of daily team operations, which \nincludes organizing practices, coordinating team travel, and overseeing logistics",
     },
