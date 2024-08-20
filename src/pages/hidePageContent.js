8ef47885ddad110d9fcc4ef46613afd3406f462e
artiehumphreys <artieh2003@gmail.com>
2024-08-20 18:39:16 -0400

    fix: refactored on click method

--- src/pages/hidePageContent.js
+++ src/pages/hidePageContent.js
@@ -1,7 +1,11 @@
 export function hidePageContent(scene) {
   const objectsToRemove = [];
   scene.children.forEach((child) => {
-    if (["icon", "text", "redirect", "image"].includes(child.userData.type)) {
+    if (
+      ["icon", "text", "redirect", "image", "dropdown", "nav"].includes(
+        child.userData.type
+      )
+    ) {
       objectsToRemove.push(child);
     }
   });
--- src/pages/home.js
+++ src/pages/home.js
@@ -5,6 +5,7 @@ import { addText } from "../utils/textUtils.js";
 
 const yPos = 42;
 const zPos = 8.85;
+let clicked = false;
 const paths = {
   "About Me": "about",
   Contact: "contact",
@@ -30,22 +31,28 @@ export function homePage(scene) {
     });
     const plane = new THREE.Mesh(planeGeometry, planeMaterial);
     plane.position.set(xPos, yPos + 2, zPos + 1.5);
-    plane.userData = { type: "icon", text: paths[name] };
+    plane.userData = { type: "nav", text: paths[name] };
     scene.add(plane);
     const iconText = await addText(xPos, yPos, zPos, name);
     scene.add(iconText);
-    createMenu(scene);
+    await createMenu(scene);
   }
 }
 
-export async function createMenu(scene) {
-  const menuGroup = new THREE.Group();
+let menuGroup;
+console.log(menuGroup);
+
+export function createMenu(scene) {
+  if (menuGroup) return;
+
+  menuGroup = new THREE.Group();
+  menuGroup.userData = { type: "dropdown" };
+
   const barWidth = 3;
   const barHeight = 0.3;
   const barSpacing = 0.6;
   const xPos = 22.2;
   const yPos = 57;
-
   const barColor = 0xffffff;
 
   const barGeometry = curvePlanes(barWidth, barHeight, xPos, yPos);
@@ -55,30 +62,20 @@ export async function createMenu(scene) {
   const bar2 = new THREE.Mesh(barGeometry, barMaterial);
   const bar3 = new THREE.Mesh(barGeometry, barMaterial);
 
-  const bars = [bar1, bar2, bar3];
+  bar1.position.set(0, barSpacing, 0);
+  bar2.position.set(0, 0, 0);
+  bar3.position.set(0, -barSpacing, 0);
 
-  bars.forEach((bar, index) => {
-    bar.position.set(0, barSpacing - barSpacing * index, 0);
-    menuGroup.add(bar);
-  });
+  menuGroup.add(bar1);
+  menuGroup.add(bar2);
+  menuGroup.add(bar3);
 
   menuGroup.position.set(xPos, yPos, 11.75);
   menuGroup.scale.set(0.7, 0.7, 0.7);
 
   scene.add(menuGroup);
 
-  let menuOpen = false;
-
-  document.addEventListener("click", () => {
-    if (menuOpen) {
-      closeMenu();
-    } else {
-      openMenu();
-    }
-    menuOpen = !menuOpen;
-  });
-
-  async function openMenu() {
+  function openMenu() {
     new TWEEN.Tween(bar1.rotation)
       .to({ z: Math.PI / 4 }, 500)
       .easing(TWEEN.Easing.Quadratic.Out)
@@ -103,20 +100,13 @@ export async function createMenu(scene) {
 
     const menuText = ["Resume", "Transcript"];
     const yStart = yPos - 1;
-    const texts = await Promise.all(
-      menuText.map((text, index) =>
-        createText(22 - text.length / 5, yStart - index * 2, text)
-      )
+    const texts = menuText.map((text, index) =>
+      createText(22 - text.length / 5, yStart - index * 2, text)
     );
 
     texts.forEach((text) => {
       scene.add(text);
     });
-  }
-
-  async function createText(xPos, yPos, name) {
-    const iconText = await addText(xPos, yPos, zPos, name, 1);
-    return iconText;
   }
 
   function closeMenu() {
@@ -151,4 +141,23 @@ export async function createMenu(scene) {
       if (text) scene.remove(text);
     });
   }
+
+  async function createText(xPos, yPos, name) {
+    const iconText = await addText(xPos, yPos, zPos, name, 1);
+    iconText.name = name;
+    return iconText;
+  }
+
+  menuGroup.onClick = () => {
+    if (clicked) {
+      closeMenu();
+    } else {
+      openMenu();
+    }
+    clicked = !clicked;
+  };
+}
+
+export function click() {
+  menuGroup.onClick();
 }
