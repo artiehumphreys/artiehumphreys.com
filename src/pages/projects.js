8f5d46b3c578d1370d36b53df5594ee429c91d2d
artiehumphreys <artieh2003@gmail.com>
2024-08-21 12:47:01 -0400

    feat: bvh to glb section

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,14 +1,46 @@
 import * as THREE from "three";
 import { curvePlanes } from "../utils/geometryUtils.js";
-export function projectPage(scene) {
-  const bvhDemo = createVideoTexture(
-    "bvh-to-glb-demo",
-    7,
-    47,
-    "github.com/artiehumphreys/bvh-to-glb?tab=readme-ov-file#video-demo"
-  );
-  scene.add(bvhDemo);
-  console.log(scene.children);
+import { addText } from "../utils/textUtils.js";
+import { createClickArea } from "./home.js";
+export async function projectPage(scene) {
+  const header = await addText(0, 56.5, 8.7, "Projects", 2);
+  await bvhGlbDemo();
+
+  scene.add(header);
+
+  async function bvhGlbDemo() {
+    const header = await addText(-18, 53.2, 8.7, "bvh-to-glb", 1.2);
+    const body = await addText(
+      -9,
+      51.2,
+      8.7,
+      "A script that converts player positional data to a multi-view \n3D web animation using the Blender API. Showcased at NBA \nTech Expo 2024.",
+      0.7
+    );
+    const skills = await addText(
+      -16.25,
+      46.2,
+      8.7,
+      "Skills: Python, JavaScript, Bash, bpy",
+      0.5
+    );
+    const bvhDemo = createVideoTexture(
+      "bvh-to-glb-demo",
+      12.75,
+      48.15,
+      "github.com/artiehumphreys/bvh-to-glb?tab=readme-ov-file#video-demo"
+    );
+    scene.add(
+      createClickArea(
+        header,
+        "redirect",
+        "github.com/artiehumphreys/bvh-to-glb"
+      )
+    );
+    [header, body, skills, bvhDemo].forEach((item) => {
+      scene.add(item);
+    });
+  }
 
   function createVideoTexture(videoName, xPos, yPos, url) {
     const video = document.createElement("video");
@@ -29,7 +61,7 @@ export function projectPage(scene) {
     const geometry = curvePlanes(width, height, xPos, yPos);
     const videoMesh = new THREE.Mesh(geometry, videoMaterial);
 
-    videoMesh.position.set(xPos, yPos, 15);
+    videoMesh.position.set(xPos, yPos, 10.4);
     videoMesh.userData = {
       type: "redirect",
       url: url,
