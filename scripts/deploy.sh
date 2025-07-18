418ccf5210f27bc663750ae93777047ed06a996d
artiehumphreys <artieh2003@gmail.com>
2025-07-17 20:43:02 -0500

    fix(scripts): updated deploy for vite dist support

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
+# https://gist.github.com/mohanpedala/1e2ff5661761d3abd0385e8223e16425?permalink_comment_id=3935570
+set -euo pipefail
 
-aws s3 sync . s3://artiehumphreys.com \
-    --exclude ".git/*" \
-    --exclude ".github/*" \
-    --exclude "README.md" \
-    --exclude ".DS_Store" \
-    --exclude ".gitignore" \
-    --exclude "eslint.config.mjs" \
-    --exclude "node_modules/*" \
-    --exclude "scripts/*.sh" \
-    --exclude "package.json" \
-    --exclude "package-lock.json" \
+npm ci
+
+echo "Building the project"
+npm run build
+
+echo "Syncing to s3://artiehumphreys.com"
+aws s3 sync dist/ s3://artiehumphreys.com \
+  --delete \
+  --exclude "Resume_Artie_Humphreys.pdf" \
+  --exclude "Summer_2025_Transcript.pdf" \
+  --cache-control "public, max-age=31536000, immutable"
+
+echo "Short caching index.html"
+aws s3 cp dist/index.html s3://artiehumphreys.com/index.html \
+  --cache-control "public, max-age=0, must-revalidate"
