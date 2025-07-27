0bc90b13c7326d1a889c8facd94b39ff7da99698
artiehumphreys <artieh2003@gmail.com>
2025-07-27 18:13:13 -0500

    feat(header): icons for external links

--- src/components/TableOfContents/index.tsx
+++ src/components/TableOfContents/index.tsx
@@ -3,7 +3,7 @@ import type { TableOfContentsProps } from "./types";
 
 const TableOfContents: React.FC<TableOfContentsProps> = ({ sections }) => {
   return (
-    <nav aria-label="Table of contents" className="font-latex prose my-8">
+    <nav aria-label="Table of contents" className="font-latex prose my-6">
       <h2 className="text-3xl font-bold mb-3 pb-2 border-b-2 border-gray-200">
         Contents
       </h2>
