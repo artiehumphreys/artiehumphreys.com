dd960adb1a3c95f90c1d872d6b4ef807b1d8184d
artiehumphreys <artieh2003@gmail.com>
2025-07-27 20:53:22 -0500

    fix(landing): fixed writeup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,5 +3,15 @@ import "katex/dist/katex.min.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <title>Artie Humphreys&apos; Blog</title>
+      <meta
+        name="description"
+        content="My personal explorations through the worlds of algorithmic optimization, creation, and beyond."
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <Component {...pageProps} />
+    </>
+  );
 }
