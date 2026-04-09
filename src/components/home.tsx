f15ecbe481ce4fe8a7b2b098c450613e1b3f87cd
artiehumphreys <artieh2003@gmail.com>
2026-04-09 10:25:51 -0400

    fix(article): improved padding, styling, and TOC for spsc post

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -15,7 +15,7 @@ export default function Home({ children }: HomeProps) {
   });
 
   return (
-    <div className="container mx-auto my-10 px-0 w-7/10 font-latex">
+    <div className="container mx-auto my-10 px-10 w-7/10 font-latex">
       <Title title="Artie Humphreys' Blog" date={formattedDate} />
 
       <article className="prose mx-auto text-lg [&>h2]:scroll-mt-28 [&>h3]:scroll-mt-28">
