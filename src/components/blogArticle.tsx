4c64cc93f78dd651f5a44fb2e609398ba29cc09c
artiehumphreys <artieh2003@gmail.com>
2026-05-08 15:15:56 -0400

    feat(article): meta tags for open graph formatting

--- src/components/blogArticle.tsx
+++ src/components/blogArticle.tsx
@@ -3,13 +3,14 @@ import Title from "./Header/index";
 
 type Props = {
   title: string;
+  description?: string;
   date?: string | Date;
   className?: string;
   children: ReactNode;
 };
 
 function formatDate(d: string | Date | undefined) {
-  const date = typeof d === "string" ? new Date(d) : d ?? new Date();
+  const date = typeof d === "string" ? new Date(d) : (d ?? new Date());
   return date.toLocaleString("en-US", {
     month: "long",
     day: "numeric",
@@ -19,20 +20,34 @@ function formatDate(d: string | Date | undefined) {
 
 export default function BlogArticle({
   title,
+  description,
   date,
   className,
   children,
 }: Props) {
   return (
-    <div className="container mx-auto my-10 px-4 md:px-10 w-full md:w-7/10 font-latex">
-      <Title title={title} date={formatDate(date)} />
-      <article
-        className={`prose mx-auto text-lg [&>h2]:scroll-mt-28 [&>h3]:scroll-mt-28 ${
-          className ?? ""
-        }`}
-      >
-        {children}
-      </article>
-    </div>
+    <>
+      <title>{title}</title>
+      <meta property="og:title" content={title} />
+      <meta property="og:type" content="article" />
+      <meta name="twitter:title" content={title} />
+      {description && (
+        <>
+          <meta name="description" content={description} />
+          <meta property="og:description" content={description} />
+          <meta name="twitter:description" content={description} />
+        </>
+      )}
+      <div className="container mx-auto my-10 px-4 md:px-10 w-full md:w-7/10 font-latex">
+        <Title title={title} date={formatDate(date)} />
+        <article
+          className={`prose mx-auto text-lg [&>h2]:scroll-mt-28 [&>h3]:scroll-mt-28 ${
+            className ?? ""
+          }`}
+        >
+          {children}
+        </article>
+      </div>
+    </>
   );
 }
